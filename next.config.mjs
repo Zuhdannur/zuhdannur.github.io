43f20074e80c59720fc76a9994224cf1f8fe9ffb
zuhdannur <pczuhdan33@gmail.com>
2025-06-01 15:52:59 +0700

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === 'production';
+console.log('is production ? ', isProd);
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
@@ -10,8 +11,8 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  ssetPrefix: isProd ? '/portofolio/' : '',
-  basePath: isProd ? '/portofolio' : '',
+  ssetPrefix: isProd ? '/zuhdannur.github.io/' : '',
+  basePath: isProd ? '/zuhdannur.github.io' : '',
   output: 'export'
 }
 
